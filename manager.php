d3be7584791133608eb926e5cb1bb53a31207833
AnhMinh245 <AT160148@actvn.edu.vn>
2023-09-21 23:00:02 +0700

    Css done kich bản 3

--- manager.php
+++ manager.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $product_image_path = $_POST['product_image_path'];
 
     $insert_query = "INSERT INTO products (tenmathang, gia, image_path) VALUES ('$product_name', $product_price, '$product_image_path')";
-    
+
     if ($conn->query($insert_query) === TRUE) {
         echo "Thêm sản phẩm mới thành công!";
     } else {
@@ -20,23 +20,38 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <!DOCTYPE html>
 <html>
+
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Trang Quản Trị</title>
+    <link rel="stylesheet" href="./css/manager.css">
 </head>
+
 <body>
-    <h2>Xin chào quản trị viên!</h2>
-    <h3>Thêm sản phẩm mới:</h3>
-    <form method="POST">
-        <label for="product_name">Tên sản phẩm:</label>
-        <input type="text" id="product_name" name="product_name" required><br><br>
+    <div class="container">
+        <form id="contact" method="POST">
+        <h3>Xin chào quản trị viên! </h3>
+        <h4> Thêm sản phẩm mới:</h4>
+            <fieldset>
+                <label for="product_name">Tên sản phẩm:</label>
+                <input placeholder="Tên sản phẩm" type="text" id="product_name" name="product_name" tabindex="1" equired autofocus ><br><br>
+            </fieldset>
 
-        <label for="product_price">Giá sản phẩm:</label>
-        <input type="number" id="product_price" name="product_price" required><br><br>
+            <fieldset>
+                <label for="product_price">Giá sản phẩm:</label>
+                <input placeholder="Giá sản phẩm" type="number" id="product_price" name="product_price" tabindex="2" equired><br><br>
+            </fieldset>
+            <fieldset>
+                <label for="product_image_path">Đường dẫn hình ảnh:</label>
+                <input type="text" id="product_image_path" name="product_image_path" tabindex="3" equired><br><br>
+            </fieldset>
 
-        <label for="product_image_path">Đường dẫn hình ảnh:</label>
-        <input type="text" id="product_image_path" name="product_image_path" required><br><br>
-
-        <input type="submit" value="Thêm sản phẩm">
-    </form>
+            <fieldset>
+                <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+            </fieldset>
+        </form>
+    </div>
 </body>
+
 </html>
